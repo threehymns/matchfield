669f5d7ab9bb43c17401ef00b651ff2a735aecc7
threehymns <70611435+threehymns@users.noreply.github.com>
2025-11-10 14:09:09 -0500

    Add Vercel analytics

--- index.tsx
+++ index.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./global.css";
+import { inject } from "@vercel/analytics";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './global.css';
+inject();
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
@@ -13,5 +15,5 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
